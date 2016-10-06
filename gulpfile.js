91e3c4a566f4e0702f9e1755668253f195e511d5
deepwest <deepwest83@mail.ru>
2016-10-06 12:50:44 +0300

    Second commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,30 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
 // --------------
+var source = {
+    css:[
+        ''
+    ],
+    js:[
+        ''
+    ],
+    sourceCss:[
+        ''
+    ],
+    sourceJs:[
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js'
+    ]
+};
 // --------------
 // Build dev.
-gulp.task('build', function(){
-
-    }
-);
+gulp.task('devjs', function () {
+    return gulp.src(source.sourceJs)
+        .pipe(newer(sourceJsDest))
+        .pipe(concat('lib.js'))
+        .pipe(jsmin())
+        .pipe(gulp.dest('assets/js/'));
+});
 //# sourceMappingURL=gulpfile.js.map
